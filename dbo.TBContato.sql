b74c93d46169272a7de263322ff9767eafdf05e9
Cristian <cristian.rr99@gmail.com>
2023-06-24 16:40:05 -0300

    implementado os primeiros exemplos com banco de dados em aplicações .net

---
+++ dbo.TBContato.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBContato] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [nome]     VARCHAR (200) NOT NULL,
+    [telefone] VARCHAR (100) NOT NULL,
+    [email]    VARCHAR (100) NOT NULL,
+    [cargo]    VARCHAR (100) NOT NULL,
+    [empresa]  VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
